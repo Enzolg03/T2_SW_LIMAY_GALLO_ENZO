c023d323d69afb4f5dc2d1406927951c6bb8fbf4
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-23 11:16:52 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/T2_SW_LIMAY_GALLO_ENZO/T2SwLimayGalloEnzoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_SW_LIMAY_GALLO_ENZO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwLimayGalloEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SwLimayGalloEnzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/T2_SW_LIMAY_GALLO_ENZO/T2SwLimayGalloEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_SW_LIMAY_GALLO_ENZO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SwLimayGalloEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
